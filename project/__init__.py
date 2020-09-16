7c4d02302d8aba1b4ec9a9818c0abdae1b6a5ba4
Iuri Severo <nobreiuri@gmail.com>
2020-09-16 14:57:25 -0300

    UnBArqDsw/2020.1_G13_iGado#48 Add postgres docker

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-import sys
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -11,6 +11,10 @@ api = Api(app)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)  
+
+# instantiate the db
+db = SQLAlchemy(app)
+
 
 class Ping(Resource):
     def get(self):
