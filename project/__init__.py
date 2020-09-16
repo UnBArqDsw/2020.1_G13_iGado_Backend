d02547b228f1b81ed476a67caf27c9706df9a9e8
Iuri Severo <nobreiuri@gmail.com>
2020-09-16 15:37:41 -0300

    UnBArqDsw/2020.1_G13_iGado#48 Fix db
    Co-authored-by: Guilherme Mendes <guimendesp12@gmail.com>

--- project/__init__.py
+++ project/__init__.py
@@ -15,6 +15,17 @@ app.config.from_object(app_settings)
 # instantiate the db
 db = SQLAlchemy(app)
 
+# model
+class Example(db.Model):  # new
+    __tablename__ = 'example'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 class Ping(Resource):
     def get(self):
