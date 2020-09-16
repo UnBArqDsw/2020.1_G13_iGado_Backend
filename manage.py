a379f224575b13def0518edd3c72c0e290feb576
Iuri Severo <nobreiuri@gmail.com>
2020-09-16 15:38:19 -0300

    UnBArqDsw/2020.1_G13_iGado#48 Add tests to the project
    Co-authored-by: Guilherme Mendes <guimendesp12@gmail.com>

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 from flask.cli import FlaskGroup
+import sys
+import unittest
 
 from project import app, db
 
@@ -12,6 +14,14 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
---
+++ project/tests/test_service.py
@@ -0,0 +1,20 @@
+import json
+import unittest
+
+from project.tests.base import BaseTestCase
+
+
+class TestService(BaseTestCase):
+    """Tests for the Service."""
+
+    def test_service(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+
+
+if __name__ == '__main__':
+    unittest.main()
