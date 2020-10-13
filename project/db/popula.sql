29fe77f3a28fb2dd4c98e29ba0d406138d089990
sudjoao <isudjoao@gmail.com>
2020-10-12 23:05:12 -0300

    UnBArqDsw/2020.1_G13_iGado#92 👌 IMPROVE: Change models and atributtes names to english

--- project/db/popula.sql
+++ project/db/popula.sql
@@ -1,26 +1,26 @@
 
-INSERT INTO PROPRIETARIO VALUES 
+INSERT INTO OWNER VALUES 
 (1, 'guimendesp12@gmail.com', 'Guilherme Mendes', 'senha'),
 (2, 'isudjoao@gmail.com', 'John Sud', 'senha5'),
 (3, 'caiovf13@gmail.com', 'Caio', 'senha4'),
 (4, 'lucasfcm@gmail.com', 'Luquita', 'senha3'),
 (5, 'severosnape@gmail.com', 'Iuri Severo Snape', 'senha2');
 
-INSERT INTO FUNCIONARIO VALUES
+INSERT INTO EMPLOYEE VALUES
 (1, 'Funcionario1', 'senhaF', 1),
 (2, 'Funcionario1', 'senhaF', 1),
 (3, 'Funcionario1', 'senhaF', 2),
 (4, 'Funcionario1', 'senhaF', 2),
 (5, 'Funcionario1', 'senhaF', 2);
 
-INSERT INTO FAZENDA VALUES
+INSERT INTO FARM VALUES
 (1, 1000, 1),
 (2, 1000, 2),
 (3, 1000, 3),
 (4, 1000, 4),
 (5, 1000, 5);
 
-INSERT INTO trabalha VALUES
+INSERT INTO work VALUES
 (1,1),
 (2,1),
 (3,2),
