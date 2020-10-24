9c77315f700dcc96f38ee4c8916ced36d92b3787
Lucas <lucasfcm9@gmail.com>
2020-10-24 16:55:06 -0300

    UnBArqDsw/2020.1_G13_iGado#113 📦 NEW: Add factory method pattern
    Co-authored-by: João Guedes <isudjoao@gmail.com>
    Co-authored-by: Iuri Severo <nobreiuri@gmail.com>

--- project/api/resources/report.py
+++ project/api/resources/report.py
@@ -5,6 +5,7 @@ from sqlalchemy import exc
 from project import db
 from project.api.models.report import GeneralReportModel
 from project.api.models.report import ReportGMDModel
+from project.api.generator_report.generator_report import ReportGenerator
 
 report_blueprint = Blueprint('ReportModel', __name__)
 api = Api(report_blueprint)
@@ -25,5 +26,17 @@ class Ping(Resource):
         'status': 'success',
         'message': 'Report!'
     }
+
+@report_blueprint.route('/report/generator_report', methods=['POST'])
+def generator_report():
+    try:
+        report_data = request.get_json()
+        generator_report = ReportGenerator()
+        generate = generator_report.generate(report_data['user_id'], report_data['type'], report_data['farm_id'])
+        return generate, 201
+    except KeyError:
+        return jsonify({'error': 'Missing parameter'}), 400
+
+    return Response({'user': user}, status=200)
 
 api.add_resource(Ping, '/reports')
