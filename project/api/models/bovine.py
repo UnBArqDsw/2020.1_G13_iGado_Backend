a0a338f8b3765198e74a7eba51909fdf8265af58
Caio Fernandes <caiovf13@gmail.com>
2020-10-26 20:31:15 -0300

    UnBArqDsw/2020.1_G13_iGado#114 📦 NEW: Add beef_cattle and dairy_cattle bovine polimorphism
    Co-authored-by: Guilherme Mendes <guimendesp12@gmail.com>

--- project/api/models/bovine.py
+++ project/api/models/bovine.py
@@ -1,40 +1,30 @@
 from project import db
+import abc
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+
+from sqlalchemy.ext.declarative import declared_attr
+
+from sqlalchemy.sql import func
 
 
-class BovineModel(db.Model):
-    __tablename__ = 'bovine'
-    id_bovine = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    farm_id = db.Column(db.Integer(), db.ForeignKey('farm.farm_id'),
-                        nullable=False)
+class BovineAbstract(db.Model):
+    __abstract__ = True
+    __metaclass__ = abc.ABCMeta
+
+    bovine_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), nullable=True)
+    date_of_birth = db.Column(db.Date(), nullable=False)
     breed = db.Column(db.String(25), nullable=True)
     actual_weight = db.Column(db.Numeric(5, 2), nullable=False)
     date_actual_weight = db.Column(db.Date(), nullable=False)
     last_weight = db.Column(db.Numeric(5, 2), nullable=True)
     date_last_weight = db.Column(db.Date(), nullable=True)
-    date_of_birth = db.Column(db.Date(), nullable=False)
+
     is_beef_cattle = db.Column(db.Boolean(), nullable=False)
 
-    def __init__(self, farm_id, breed, actual_weight,
-                 date_actual_weight, last_weight, date_last_weight,
-                 date_of_birth, is_beef_cattle):
-        self.farm_id = farm_id
-        self.breed = breed
-        self.actual_weight = actual_weight
-        self.date_actual_weight = date_actual_weight
-        self.last_weight = last_weight
-        self.date_last_weight = date_last_weight
-        self.date_of_birth = date_of_birth
-        self.is_beef_cattle = is_beef_cattle
+    @declared_attr
+    def farm_id(cls): 
+        return db.Column(db.Integer, db.ForeignKey('farm.farm_id'))
 
-    def to_json(self):
-        return {
-            'id_bovine': self.id_bovine,
-            'farm_id': self.farm_id,
-            'breed': self.breed,
-            'actual_weight': float(self.actual_weight),
-            'last_weight': float(self.last_weight),
-            'date_last_weight': str(self.date_last_weight),
-            'date_actual_weight': str(self.date_actual_weight),
-            'date_of_birth': str(self.date_of_birth),
-            'is_beef_cattle': self.is_beef_cattle
-        }
+    def __init__(self, farm_id):
+        self.farm_id = farm_id
--- project/api/resources/bovine.py
+++ project/api/resources/bovine.py
@@ -1,44 +1,20 @@
 from flask import Blueprint, request, jsonify
-from flask_restful import Api
+from flask_restful import Api, Resource
 from project import db
-from project.api.models.bovine import BovineModel
+from project.api.models.beef_cattle import BeefCattle
+from project.api.models.dairy_cattle import DairyCattle
 from flask_jwt_extended import jwt_required
 
 bovine_blueprint = Blueprint('bovine', __name__)
 api = Api(bovine_blueprint)
 
 
-@bovine_blueprint.route('/bovine', methods=['POST'])
-@jwt_required
-def create_bovine():
-    bovine_data = request.get_json()
-
-    bovine = BovineModel(farm_id=bovine_data['farm_id'],
-                         breed=bovine_data['breed'],
-                         actual_weight=bovine_data['actual_weight'],
-                         date_actual_weight=bovine_data['date_actual_weight'],
-                         last_weight=bovine_data['last_weight']
-                         if len(bovine_data['last_weight']) > 1 else None,
-                         date_last_weight=bovine_data['date_last_weight']
-                         if len(bovine_data['date_last_weight']) > 1 else None,
-                         date_of_birth=bovine_data['date_of_birth'],
-                         is_beef_cattle=bovine_data['is_beef_cattle'])
-    try:
-        db.session.add(bovine)
-        db.session.commit()
-    except ValueError:
-        return jsonify(
-            {"erro": "DB error, was not possible not save the bovine"}), 404
-
-    return jsonify({"message": "Bovine created successfully!"}), 200
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'Report!'
+        }
 
 
-@bovine_blueprint.route('/bovine', methods=['GET'])
-@jwt_required
-def get_bovines():
-    try:
-        bovines = BovineModel.query.all()
-    except Exception:
-        return jsonify({"erro": "Database error"}), 404
-    bovine_json = [BovineModel.to_json(bovine) for bovine in bovines]
-    return jsonify(bovine_json), 200
+api.add_resource(Ping, '/reports')
